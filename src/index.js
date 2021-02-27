3145964099e04f2f58edc32bf97c72dbdb1bda0a
Naumov Evgeniy <n_zhenek@mail.ru>
2021-02-27 15:56:29 +0400

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+  let result ="";
+  let strnum  = String(n).split("");
+  strnum = strnum.reverse();
+  let reverseNumber = strnum.toString();
+  for (let i = 0; i < reverseNumber.length; i++)
+  {
+    if (reverseNumber[i] === "," || reverseNumber[i] === "-" ) {}
+    else {
+        result += reverseNumber[i];
+    }
+  }
+  result = Number(result)
+  return result;
 }
